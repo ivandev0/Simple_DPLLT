28b3266d879e9eba3faf54f3d1a4df5505e3e253
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2019-12-01 16:52:09 +0300

    Write simple test for EUF formulas parsing

--- src/test/java/reader/EUFReaderTest.java
+++ src/test/java/reader/EUFReaderTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class EUFReaderTest {
     @Test
     public void test() {
-        Formula equalities = EUFReader.transform("f(d) = a ^ b = d(s, e)");
-        System.out.println(equalities);
+        Formula actual = EUFReader.transform("f(n) = a ^ b = d(s(a, b), e) ^ b != f(n)");
+        System.out.println(actual);
     }
 }
